bd87f3efaa172c6b89b04396cb25f17022c2c7a2
lyf_gh <156231817@qq.com>
2022-09-19 22:25:10 +0800

    update download script

--- download_REUnet_trained_models.py
+++ download_REUnet_trained_models.py
@@ -1,18 +1,21 @@
 import requests
 import os
+import sys
 from tqdm import tqdm
 
 
 def main():
     # set parameter
     url = 'https://zenodo.org/record/7091314/files/REUNet_trained_models.zip?download=1'
-    zip_download_path = '/gpfs/lab/liangmeng/members/liyifan/others/test/nnu_cp_test/download_REU.zip'
+    zip_download_path = os.path.join(sys.path[0], 'REUnet_trained_models.zip')
 
     # nnUnet trained model path
     trained_model_path = os.getenv('RESULTS_FOLDER')
     assert trained_model_path is not None, 'nnUnet RESULTS_FOLDER Not Found!'
 
     # download
+    print('Temporary files will be downloaded to\n\t{}'.format(zip_download_path))
+    print('And will be removed afterd unzip.')
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with tqdm.wrapattr(open(zip_download_path, "wb"), "write", miniters=1,
